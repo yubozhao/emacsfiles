b28184545c43cfcfbc664ce51f3c94765c76b470
Bozhao Yu <yubz86@gmail.com>
2013-08-07 01:15:24 -0700

    Add evil. Modify init

---
+++ elpa/evil-20130804.1513/evil-pkg.el
@@ -0,0 +1,3 @@
+(define-package "evil" "20130804.1513" "Extensible Vi layer for Emacs. [git]"
+  '((undo-tree "0.6.3")))
+
--- init.el
+++ init.el
@@ -6,9 +6,52 @@
 
 ;;; load path
 (require 'package)
+(require 'whitespace)
 (package-initialize)
 ;;; END
 
 ;;; init stuff
 (load-theme 'solarized-dark t)
+;;; END
 
+;;; Slime
+(add-to-list 'load-path "/opt/local/share/emacs/site-lisp/slime")
+(setq slime-lisp-implementations
+           `((sbcl ("/opt/local/bin/sbcl"))
+                    (abcl ("/opt/local/bin/abcl"))
+                           (clisp ("/opt/local/bin/clisp"))))
+(require 'slime)
+(slime-setup  '(slime-repl slime-asdf slime-fancy slime-banner))
+;;; END
+
+;; On the fly syntax checking
+(require 'flycheck)
+(global-flycheck-mode)
+(setq flycheck-highlighting-mode 'lines)
+
+;; Configure whitespace settings to display when we are over 80chars
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-line-column 79)
+(global-whitespace-mode 1)
+(setq-default fill-column 79)
+
+ ;; no startup msg
+(setq inhibit-startup-message t)
+;
+; turn on paren match highlighting
+(show-paren-mode 1)
+;
+; highlight entire bracket expression
+(setq show-paren-style 'expression)
+;
+; display line numbers in margin
+(global-linum-mode 1)
+;
+; display the column and line our cursor is on
+(column-number-mode 1)
+;
+; stop creating those backup~ files
+(setq make-backup-files nil)
+;
+; stop creating those #autosave# files
+(setq auto-save-default nil)
