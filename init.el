0536f7973434932834ab8ca793e47d35b86973df
Bozhao Yu <yubozhao@Bozhaos-MacBook-Air.local>
2013-08-06 23:27:09 -0700

    Add magic and solarized

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+;;; package sources
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+;;; END
+
+;;; load path
+(require 'package)
+(package-initialize)
+;;; END
+
+;;; init stuff
+(load-theme 'solarized-dark t)
+
